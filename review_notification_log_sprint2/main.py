92cf5f91ebcf15b0d5b70b87acb2ee26375c0395
jasag1137 <156482912+jasag1137@users.noreply.github.com>
2025-05-31 09:40:44 -0700

    No changes main.py

---
+++ review_notification_log_sprint2/main.py
@@ -0,0 +1,15 @@
+# ***************************************************************
+"""
+Author(s): jasag
+Creation Date: 2025-04-23
+Last Modified: 2025-05-05
+
+Description:
+This file is the entry point of the application.
+"""
+# ***************************************************************
+
+from gui.search_logs_gui import run_app
+
+if __name__ == "__main__":
+    run_app()
